ccd9386ce97173644011d98de342c3088ffe35f9
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2021-11-11 13:59:51 +0800

    created small card variation for all bookings page
    sorts all bookings page according to date

--- src/mobile/features/allBookings/components/index.tsx
+++ src/mobile/features/allBookings/components/index.tsx
@@ -1,37 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "../../../../shared/Header";
 import BottomAppBar from "../../../../shared/BottomAppBar";
 import SmallCard from "../../../../shared/SmallCard";
 import * as helper from "../../../../utils/helper";
+import { IBookingItem } from "../../../../interfaces";
 
 const Wrapper: React.FC<any> = (props) => {
-  return (
-    <>
-      <div className="bg-defaultPinkBg h-screen overflow-scroll">
-        <Header type={"all"} showBackBtn={true} title={"All Bookings"} />
+    const [bookingsList, setBookingsList] = useState([]);
+    const [datesList, setDatesList] = useState([]);
 
-        <div className="p-4">
-          {helper.SampleBookings.map((booking, index) => {
-            return (
-              <SmallCard
-                key={index}
-                type={"allBookings"}
-                packageType={booking.packageType}
-                sender={booking.from}
-                location={booking.location}
-                date={booking.date}
-                status={booking.status}
-                price={booking.amount}
-              />
-            );
-          })}
-        </div>
+    const groupBookingsByDate = (list: IBookingItem[]): void => {
+        let dates: string[] = [];
+        list.map((booking) => {
+            dates.push(booking.date);
+        });
 
-        <BottomAppBar />
-      </div>
-    </>
-  );
+        setDatesList([...new Set(dates)]);
+        setBookingsList(list);
+    };
+
+    useEffect(() => {
+        groupBookingsByDate(helper.SampleBookings);
+    }, []);
+    return (
+        <>
+            <div className="bg-defaultPinkBg h-screen overflow-scroll">
+                <Header
+                    type={"all"}
+                    showBackBtn={true}
+                    title={"All Bookings"}
+                />
+
+                <div className="p-4">
+                    {datesList.map((date, index) => {
+                        return (
+                            <div>
+                                <div
+                                    key={index}
+                                    className="text-defaultBlack text-sm font-poppins tracking-wider"
+                                >
+                                    {date}
+                                </div>
+                                <div>
+                                    {bookingsList.map((booking, index) => {
+                                        if (booking.date === date) {
+                                            return (
+                                                <SmallCard
+                                                    key={index}
+                                                    type={"allBookings"}
+                                                    packageType={
+                                                        booking.packageType
+                                                    }
+                                                    sender={booking.from}
+                                                    location={booking.location}
+                                                    date={booking.date}
+                                                    status={booking.status}
+                                                    price={booking.amount}
+                                                />
+                                            );
+                                        } else {
+                                            return;
+                                        }
+                                    })}
+                                </div>
+                            </div>
+                        );
+                    })}
+                    {}
+                </div>
+
+                <BottomAppBar />
+            </div>
+        </>
+    );
 };
 
 export default Wrapper;
--- src/shared/SmallCard.tsx
+++ src/shared/SmallCard.tsx
@@ -4,20 +4,20 @@ import Cartrunk from "../img/cartrunk-small.svg";
 import Delivery from "../img/delivery-small.svg";
 
 interface SmallCardProps {
-  /** template type of card to be used */
-  type: string;
-  /** either cartrunk or delivery package type */
-  packageType: string;
-  /** sender of the package */
-  sender: string;
-  /** location of the receiver */
-  location: string;
-  /** booked date for the package */
-  date?: string;
-  /** total price of the package */
-  price?: string | number;
-  /** current status of the package */
-  status?: string;
+    /** template type of card to be used */
+    type: string;
+    /** either cartrunk or delivery package type */
+    packageType: string;
+    /** sender of the package */
+    sender: string;
+    /** location of the receiver */
+    location: string;
+    /** booked date for the package */
+    date?: string;
+    /** total price of the package */
+    price?: string | number;
+    /** current status of the package */
+    status?: string;
 }
 
 /** Component for small-sized card
@@ -26,67 +26,67 @@ interface SmallCardProps {
  *
  */
 const SmallCard: React.FC<SmallCardProps> = ({
-  type,
-  packageType,
-  sender,
-  location,
-  date,
-  price,
-  status,
+    type,
+    packageType,
+    sender,
+    location,
+    date,
+    price,
+    status,
 }) => {
-  /** Conditionally render card details according to type */
-  const renderCard = () => {
-    if (type === "today") {
-      return (
-        <div className="text-xs text-opacity-60 text-defaultBlack flex items-center">
-          <div className="flex">{date}</div>
-        </div>
-      );
-    } else if (type === "allBookings") {
-      return (
-        <div className="flex flex-col">
-          <div className="p-1 font-poppins text-center font-semibold text-xs text-purple3">
-            Php {price}.00
-          </div>
-          <div
-            className={`w-24 h-5 p-1 rounded-2xl flex items-center justify-center font-poppins text-xs font-medium tracking-wider text-defaultWhite ${
-              status === "IN PROCESS"
-                ? "bg-statusBlue"
-                : status === "DELIVERED"
-                ? "bg-statusGreen"
-                : "bg-statusRed"
-            }`}
-          >
-            {status}
-          </div>
-        </div>
-      );
-    }
-  };
+    /** Conditionally render card details according to type */
+    const renderCard = () => {
+        if (type === "today") {
+            return (
+                <div className="text-xs text-opacity-60 text-defaultBlack flex items-center">
+                    <div className="flex">{date}</div>
+                </div>
+            );
+        } else if (type === "allBookings") {
+            return (
+                <div className="flex flex-col">
+                    <div className="p-1 font-poppins text-center font-semibold text-xs text-purple3">
+                        Php {price}.00
+                    </div>
+                    <div
+                        className={`w-24 h-5 p-1 rounded-2xl flex items-center justify-center font-poppins text-xs font-medium tracking-wider text-defaultWhite ${
+                            status === "IN PROCESS"
+                                ? "bg-statusBlue"
+                                : status === "DELIVERED"
+                                ? "bg-statusGreen"
+                                : "bg-statusRed"
+                        }`}
+                    >
+                        {status}
+                    </div>
+                </div>
+            );
+        }
+    };
 
-  return (
-    <>
-      <div className="my-2 flex bg-defaultWhite py-2 px-4 rounded-xl justify-between">
-        <div className="flex">
-          <div className="w-16 flex items-center">
-            {packageType === "cartrunk" ? (
-              <img src={Cartrunk} alt={packageType} />
-            ) : (
-              <img src={Delivery} alt={packageType} />
-            )}
-          </div>
-          <div className="pl-3">
-            <div className="text-lg font-medium">{sender}</div>
-            <div className="text-xs text-opacity-60 text-defaultBlack overflow-hidden w-34 h-4">
-              {location}
+    return (
+        <>
+            <div className="my-2 flex bg-defaultWhite py-2 px-4 rounded-xl justify-between">
+                <div className="flex">
+                    <div className="w-16 flex items-center">
+                        {packageType === "cartrunk" ? (
+                            <img src={Cartrunk} alt={packageType} />
+                        ) : (
+                            <img src={Delivery} alt={packageType} />
+                        )}
+                    </div>
+                    <div className="pl-3">
+                        <div className="text-lg font-medium">{sender}</div>
+                        <div className="text-xs text-opacity-60 text-defaultBlack overflow-hidden w-34 h-4">
+                            {location}
+                        </div>
+                    </div>
+                </div>
+
+                {renderCard()}
             </div>
-          </div>
-        </div>
-
-        {renderCard()}
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default SmallCard;
