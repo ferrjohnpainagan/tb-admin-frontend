4817c67314fe70efc13683538d13ff63d651a4ea
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2021-11-21 13:31:55 +0800

    updates tailwind colors

--- src/shared/CircleColor.tsx
+++ src/shared/CircleColor.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import * as helper from "../utils/helper";
 
 interface ColorProps {
-    /** Theme for the package */
-    color: string;
+  /** Theme for the package */
+  color: string;
 }
 
 /** Circle shape component for theme
@@ -11,17 +11,17 @@ interface ColorProps {
  * @component
  */
 const CircleColor: React.FC<ColorProps> = ({ color }) => {
-    return (
-        <div className="flex flex-col text-defaultBlack ">
-            <div
-                className={`w-5 h-5 rounded-xl mt-2  bg-${helper.parseColorTheme(
-                    color
-                )}`}
-            ></div>
+  return (
+    <div className="flex flex-col text-defaultBlack">
+      <div
+        className={`w-5 h-5 rounded-xl mt-2 bg-${helper.parseColorTheme(
+          color
+        )}`}
+      ></div>
 
-            {color}
-        </div>
-    );
+      {color}
+    </div>
+  );
 };
 
 export default CircleColor;
