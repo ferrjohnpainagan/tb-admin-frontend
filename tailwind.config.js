6c308df7692167b446802af84919cbc9e3796014
ferrjohnpainagan <ferrjohnpainagan@gmail.com>
2021-11-21 13:52:15 +0800

    removes purge on tailwind prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  // purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
